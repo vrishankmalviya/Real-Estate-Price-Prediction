2beb4d7d967fbc03109b5c421b31c8a2cdea5ee6
vrishankmalviya <vrishankmalviya@gmail.com>
2024-03-23 13:44:48 +0530

    update

--- Server/util.py
+++ Server/util.py
@@ -1,9 +1,31 @@
 import json
 import pickle
+import numpy as np
+
 
 __location =None
 __data_columns=None
 __model=None
+
+
+def price(location,total_sqft,bath, balcony, bhk):
+    
+     loc_index = np.where(X.columns==location)[0][0]
+
+    x = np.zeros(len(X.columns))
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
+    x[3] =balcony
+    if loc_index >= 0:
+        x[loc_index] = 1
+    
+    try:
+        loc_index = __data_columns.index(location.lower())
+    except:
+        loc_index = -1
+    return __model.predict([x])
+    
 
 
 def get_location():
@@ -15,15 +37,18 @@ def load():
     global __location
     #global __model
     
+    
+    
     with open("Server/artifacts/columns.json",'r')as f:
-       __data_columns= json.load(f)
-       __location = __data_columns[3:] 
+       __data_columns= json.load(f)['data_columns']
+       __location = __data_columns[4:] 
        
        
-    with open("Server\artifacts\Real_Estate_model.pickle",'rb') as f:
+    with open("Server/artifacts/Real_Estate_model.pickle",'rb') as f:
         __model = pickle.load(f)
     print("Data columns loaded successfully.")
     
 
 if __name__ == '__main__':
+    load()
     print(get_location())
